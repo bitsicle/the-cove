85e59a5979e48111670c87bcd430558ea5651c6d
Bitsicle <denmalmin@gmail.com>
2015-09-28 22:40:26 +0200

    Small update, just added some files. Remember to add mongoose and database files

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
 exports.index = function(req, res) {
-	res.render('default');
-};
+	res.render('default', {
+		title: 'The Cove'
+	});
+}
+
+exports.search = function(req, res) {
+	res.render('search', {
+		title: 'Results'
+	});
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', routes.index);
+app.get('/search', routes.search);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
