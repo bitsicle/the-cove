b34e9d0bc054f3c385f928e2abcf074e885084ce
Bitsicle <denmalmin@gmail.com>
2015-09-29 15:28:01 +0200

    Added a small spider file which is a test. Also fixed the require(casper) variable. Removed login options and instead added several links for various modes.

--- spiders/piratebay_spider.js
+++ spiders/piratebay_spider.js
@@ -1,1 +1,25 @@
-var casper = require('casperjs');
+var casper = require('casper').create();
+var x = require('casper').selectXPath;
+
+casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
+
+casper.start('http://thepiratebeach.eu')
+
+casper.then(function() {
+  this.sendKeys('input[name="q"]', 'Daft Punk');
+  console.log('Entering data');
+});
+
+casper.thenClick(x('//*[@id="subm"]/input[1]'), function() {
+  console.log('Searching thepiratebeach.eu');
+});
+
+casper.wait(500, function() {
+  casper.click(x('//*[@id="searchResult"]/tbody/tr[2]/td[2]/div/a'));
+});
+
+casper.wait(500, function() {
+  casper.capture('testImage.png')
+});
+
+casper.run();
